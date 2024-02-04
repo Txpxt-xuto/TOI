26b7275fd625e2a97547f02b68960eec87637276
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-02-04 12:46:06 +0700

    แบ่งหลักของเลขในหน้าที่ d

--- Roman_TOI48.c
+++ Roman_TOI48.c
@@ -1,5 +1,25 @@
+/*
+TASK: ROMAN
+AUTHOR: Tapat Toungsakul
+CENTER: Home
+*/
 #include <stdio.h>
 int main()
 {
-    printf("auto hee");
+    int d,i=0,v=0,x=0,l=0,c=0;
+    scanf("%d",&d);
+    while (d>=1)
+    {
+        if(d/100>=1)
+        {
+            c+=d/100;
+        }
+        else if(d%10==9)
+        {
+            i+=1;
+            x+=1;
+        }
+        d-=1;
+    }
+    printf("%d %d %d %d %d",i,v,x,l,c);
 }
