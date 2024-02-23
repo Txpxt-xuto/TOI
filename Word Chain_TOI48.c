83ca62725822ccdf0ee03b4454166928b79308f2
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-02-23 20:52:45 +0700

    Create Word Chain_TOI48.c

---
+++ Word Chain_TOI48.c
@@ -0,0 +1,37 @@
+/*
+Lang: C
+TASK: CHAIN
+AUTHOR: Tapat Toungsakul
+CENTER: Home
+*/
+#include <stdio.h>
+int main()
+{
+    int L,N,x,y,z;
+    scanf("%d",&L);
+    scanf("%d",&N);
+    char word[N][L];
+    for(x=0;x<N;x++)
+    {
+        scanf("%s",&word[x]);
+    }
+    for(x=0;x<N-1;x++)
+    {
+        z=0;
+        for(y=0;y<L;y++)
+        {
+            if(word[x][y]!=word[x+1][y])
+            {
+                z+=1;
+            }
+        }
+        if(z>2)
+        {
+            for(y=0;y<L;y++)
+            {
+                printf("%c",word[x][y]);
+            }
+            break;
+        }
+    }
+}
