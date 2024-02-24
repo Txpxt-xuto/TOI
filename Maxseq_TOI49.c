16e22e51d4f8b2fb1f1ff534c7f4177aeb99b39f
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-02-24 12:47:12 +0700

    Create Maxseq_TOI49.c

---
+++ Maxseq_TOI49.c
@@ -0,0 +1,47 @@
+/*
+TASK: MAXSEQ
+LANG: C
+AUTHOR: Tapat Toungsakul
+CENTER: Home
+*/
+#include <stdio.h>
+int main()
+{
+    int i,j,k,n,sum,max=0,x,y,z;
+    scanf("%d",&n);
+    int Main_Sequences[n];
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&Main_Sequences[i]);
+    }
+    for (i=0;i<n;i++)
+    {
+        for (j=i;j<n;j++)
+        {
+            sum=0;
+            for (k=i;k<=j;k++)
+            {
+                sum+=Main_Sequences[k];
+            }
+            if(max<sum)
+            {
+                max=sum;
+                x=i;
+                y=j;
+                z=k;
+            }
+        }
+    }
+    if(max<=0)
+    {
+        printf("Empty sequence");
+    }
+    else if(max>0)
+    {
+        printf("%d\n",max);
+        for(z=x;z<=y;z++)
+        {
+            printf("%d ",Main_Sequences[z]);
+        }
+    }
+}
