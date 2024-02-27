9fd1ed11ce787c3ea32f08e1081c5fb90a388ddf
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-02-27 19:23:14 +0700

    Update Bee_TOI49.c

--- Bee_TOI49.c
+++ Bee_TOI49.c
@@ -3,42 +3,37 @@ TASK: BEE
 LANG: C
 AUTHOR: Tapat Toungsakul
 CENTER: Home
-
+*/
 #include <stdio.h>
 int main()
 {
-    int i,j,k;
+    // input : i j -1
+    int i,j,k,x;
+    int fibo1=0,fibo2=1,fibo=fibo1+fibo2;
+    int bee1=4,bee=bee1;
     scanf("%d %d %d",&i,&j,&k);
-
-}*/
-#include <stdio.h>
-
-int main(void){
-	short int years[24] = {0};
-	int n = 0, answer = 0;
-	do{
-		scanf("%d", &answer);
-		years[n++] = answer;
-		
-	} while (answer != -1);
-	for(int i = 0; i < n - 1; i++){
-		int year = years[i];
-		int bees[3] = {1, 1, 0};
-		while(year--){
-			int worker_dead = bees[1];
-			int soldier_dead = bees[2];
-			bees[1]++;
-			for(int j = 0; j < worker_dead; j++){
-				bees[1]++;
-				bees[2]++;
-			}
-			for(int j = 0; j < soldier_dead; j++){
-				bees[1]++;
-			}
-			bees[1] -= worker_dead;
-			bees[2] -= soldier_dead;
-		}
-		printf("%d %d\n", bees[1], bees[0] + bees[1] + bees[2]);
-	}
-	return 0;
+    for (x=1;x<i+1;x++) 
+    {
+        fibo1 = fibo2;
+        fibo2 = fibo;
+        fibo = fibo1 + fibo2;
+        if(fibo>=3)
+        {
+            bee=bee1+fibo;
+            bee1=bee;
+        }
+    }
+    printf("%d %d\n",bee-fibo,bee);//คำตอบของปี i
+    for (x=1;x<j;x++) 
+    {
+        fibo1 = fibo2;
+        fibo2 = fibo;
+        fibo = fibo1 + fibo2;
+        if(fibo>=3)
+        {
+            bee=bee1+fibo;
+            bee1=bee;
+        }
+    }
+    printf("%d %d",bee-fibo,bee);//คำตอบของป j
 }
