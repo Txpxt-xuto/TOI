a1303c9e446eb3eaa1dbd41b79311bc214d58931
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-03 15:33:56 +0700

    Final

--- Bee_TOI49.c
+++ Bee_TOI49.c
@@ -7,38 +7,19 @@ CENTER: Home
 #include <stdio.h>
 int main()
 {
-    // input : i j -1
-    int i,j,k,x;
-    int fibo1=0,fibo2=1,fibo=fibo1+fibo2;
-    int bee1=4,bee=bee1;
-    scanf("%d %d %d",&i,&j,&k);
-    for (x=1;x<i+1;x++) 
+    int n,a,b,i,j;
+    scanf("%d", &n);
+    while (n != -1) 
     {
-        fibo1 = fibo2;
-        fibo2 = fibo;
-        fibo = fibo1 + fibo2;
-        if(fibo>=3)
+        a = 1;
+        b = 0;
+        for (i=0;i<n;i++)
         {
-            bee=bee1+fibo;
-            bee1=bee;
+            j = b;
+            b = a;
+            a = a + 1 + j;
         }
+        printf("%d %d\n", a, a + b + 1);
+        scanf("%d", &n);
     }
-    printf("%d %d\n",bee-fibo,bee);//คำตอบของปี i โดยเป็น จำนวนมดงาน จำนวนมดทั้งหมด
-    fibo1=0;
-    fibo2=1;
-    fibo=fibo1+fibo2;
-    bee1=4;
-    bee=bee1;
-    for (x=1;x<j+1;x++) 
-    {
-        fibo1 = fibo2;
-        fibo2 = fibo;
-        fibo = fibo1 + fibo2;
-        if(fibo>=3)
-        {
-            bee=bee1+fibo;
-            bee1=bee;
-        }
-    }
-    printf("%d %d",bee-fibo,bee);//คำตอบของป j โดยเป็น จำนวนมดงาน จำนวนมดทั้งหมด
 }
