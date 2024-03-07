f1c066132e2e812f90c259a51ede20f6afeb340c
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-07 16:17:58 +0700

    Create Word_TOI49.c

---
+++ Word_TOI49.c
@@ -0,0 +1,33 @@
+/*
+Lang: C
+TASK: Word
+AUTHOR: Tapat Toungsakul
+CENTER: Home
+*/
+#include <stdio.h>
+int main()
+{
+    int n,m,i,j,k,x;
+    scanf("%d %d",&n,&m);
+    char word[n][m];
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",&word[i]);
+    }
+    scanf("%d",&x);
+    char sentense[x][15];
+    for(i=0;i<x;i++)
+    {
+        scanf("%s",&sentense[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(word[i][j]==sentense[i][j])
+            {
+
+            }
+        }
+    }
+}
