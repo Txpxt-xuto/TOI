c6f0ab5e1b4e2060edcc6638e50a973fe032acfa
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-11 13:32:27 +0700

    Final

--- Pattern_TOI48.c
+++ Pattern_TOI48.c
@@ -4,7 +4,35 @@ AUTHOR: Tapat Toungsakul
 CENTER: Home
 */
 #include <stdio.h>
+char PATTERN[50010][100] = {};
+int i,j,k,x,y,z;
+int max = 0;
 int main()
 {
-    
+    int N;
+	scanf("%d",&N);
+	for(i=1;i<=50000;i++)
+    {
+		for(j=1;j<=70;j++)
+        {
+			PATTERN[i][j] = 'o';
+		}
+	}
+    for(i=0;i<N;i++){
+		scanf("%d %d %d",&x,&y,&z);
+		for(j=0;j<z;j++){
+			PATTERN[x][y+j] = 'x';
+		}
+		if(x > max){
+			max = x;
+		}
+	}
+    for(i=1;i<=max;i++)
+    {
+		for(j=1;j<=70;j++)
+        {
+			printf("%c",PATTERN[i][j]);
+		}
+		printf("\n");
+	}
 }
