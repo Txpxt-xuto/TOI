8d085ddcdb8b8542ba78b513c0898b2f56a7cf7f
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-21 20:42:20 +0700

    Update TOI5_Jump.c

--- TOI5_Jump.c
+++ TOI5_Jump.c
@@ -8,5 +8,14 @@ CENTER: YourCenter
 unsigned int x[30000];
 int main()
 {
-
+    int N,K,i,j;
+    scanf("%d %d",&N,&K);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+        printf("%d ",x[i]);
+    }
 }
