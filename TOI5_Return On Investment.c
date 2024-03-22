e1fa2a271c9772eb038e32f44ae2c38be57d49ec
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-22 17:18:53 +0700

    Update TOI5_Return On Investment.c

--- TOI5_Return On Investment.c
+++ TOI5_Return On Investment.c
@@ -8,16 +8,26 @@ CENTER: Home
 #include <math.h>
 int main()
 {
-    int Fixed_cost,Variable_cost,profit,income,Total_cost,Number_of_books_sold,Selling_price_per_book,Number_of_prints,D;
-    scanf("%d",&Fixed_cost);
+    double Fixed_cost,Variable_cost,profit,income,Total_cost,Number_of_books_sold,Selling_price_per_book,Number_of_prints,D,min=1000;
+    double A,B,C;
+    scanf("%lf",&Fixed_cost);
     for(Number_of_prints=1000;Number_of_prints<=15000;Number_of_prints+=500)
     {
-        for(Selling_price_per_book=74;Selling_price_per_book<=114;Selling_price_per_book++)
+        for(Selling_price_per_book=74;Selling_price_per_book<=144;Selling_price_per_book++)
         {
             Variable_cost = Number_of_prints + (102 - (Number_of_prints/500));
             Total_cost = Fixed_cost + Variable_cost;
             D = Selling_price_per_book-100;
-            Number_of_books_sold =  ((100 - (0.8569 * exp(0.09*D))) * จํานวนหนังสือที่พิมพ์ ) / 100
+            Number_of_books_sold =  ((100 - (0.8569 * exp(0.09*D))) * Number_of_prints ) / 100;
+            income = Number_of_books_sold * Selling_price_per_book;
+            profit = income - Total_cost;
+            if(profit >= 0 && min>profit)
+            {
+                min = profit;
+                A = Number_of_prints;
+                B = Selling_price_per_book;
+                C;
+            }
         }
     }
 
