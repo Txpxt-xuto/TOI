4a5ae8d2e03de4b6ccaa2ce4a5bbed24f2cfc55d
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-22 17:43:18 +0700

    Update TOI5_Return On Investment.c

--- TOI5_Return On Investment.c
+++ TOI5_Return On Investment.c
@@ -8,7 +8,7 @@ CENTER: Home
 #include <math.h>
 int main()
 {
-    double Fixed_cost,Variable_cost,profit,income,Total_cost,Number_of_books_sold,Selling_price_per_book,Number_of_prints,D,min=9999999999999;
+    double Fixed_cost,Variable_cost,profit,income,Total_cost,Number_of_books_sold,Selling_price_per_book,Number_of_prints,min=9999999999999;
     double A,B,C;
     scanf("%lf",&Fixed_cost);
     for(Number_of_prints=1000;Number_of_prints<=15000;Number_of_prints+=500)
@@ -17,8 +17,7 @@ int main()
         {
             Variable_cost = Number_of_prints + (100 - ((Number_of_prints-1000)/500));
             Total_cost = Fixed_cost + Variable_cost;
-            D = Selling_price_per_book-100;
-            Number_of_books_sold =  ((100 - (0.8569 * exp(0.09*D))) * Number_of_prints ) / 100;
+            Number_of_books_sold =  ((100 - (0.8569 * exp(0.09*(Selling_price_per_book-100)))) * Number_of_prints ) / 100;
             income = Number_of_books_sold * Selling_price_per_book;
             profit = income - Total_cost;
             printf("%f %f %f %f %f %f %f %f %f \n",Fixed_cost,Variable_cost,profit,income,Total_cost,Number_of_books_sold,Selling_price_per_book,Number_of_prints,D);
@@ -31,5 +30,5 @@ int main()
             }*/
         }
     }
-    printf("%.0f %.0f %.2f",A,B,C);
+    printf("%.0f\n%.0f\n%.2f",A,B,C);
 }
