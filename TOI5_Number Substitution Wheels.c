01fc267d2617b0ba3a3323d222b9077a71e3ca9a
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-22 21:12:13 +0700

    Create TOI5_Number Substitution Wheels.c

---
+++ TOI5_Number Substitution Wheels.c
@@ -0,0 +1,15 @@
+/*
+TASK: NSW
+LANG: C
+AUTHOR: Tapat Toungsakul
+CENTER: Home
+*/
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char input[3],password[256];
+    scanf("%s\n%s",&input,&password);
+    int i,j,k;
+    
+}
