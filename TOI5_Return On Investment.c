4c41e57b5fb7e9a7fbb539c04bd14b55ce347be2
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-22 17:10:17 +0700

    Update TOI5_Return On Investment.c

--- TOI5_Return On Investment.c
+++ TOI5_Return On Investment.c
@@ -8,6 +8,17 @@ CENTER: Home
 #include <math.h>
 int main()
 {
-    int Fixed_cost,Variable_cost,profit,income,Total_cost,Number_of_books_sold,Selling_price_per_book;
+    int Fixed_cost,Variable_cost,profit,income,Total_cost,Number_of_books_sold,Selling_price_per_book,Number_of_prints,D;
     scanf("%d",&Fixed_cost);
+    for(Number_of_prints=1000;Number_of_prints<=15000;Number_of_prints+=500)
+    {
+        for(Selling_price_per_book=74;Selling_price_per_book<=114;Selling_price_per_book++)
+        {
+            Variable_cost = Number_of_prints + (102 - (Number_of_prints/500));
+            Total_cost = Fixed_cost + Variable_cost;
+            D = Selling_price_per_book-100;
+            Number_of_books_sold =  ((100 - (0.8569 * exp(0.09*D))) * จํานวนหนังสือที่พิมพ์ ) / 100
+        }
+    }
+
 }
