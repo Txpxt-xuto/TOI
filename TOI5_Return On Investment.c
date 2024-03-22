a8ba3a5e7f197d3fef9146b0eceb31d69af44fd3
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-22 16:58:15 +0700

    Create TOI5_Return On Investment.c

---
+++ TOI5_Return On Investment.c
@@ -0,0 +1,28 @@
+/*
+TASK: ROI
+LANG: C
+AUTHOR: Tapat Toungsakul
+CENTER: Home
+*/
+#include <stdio.h>
+unsigned int x[30005];
+int main()
+{
+    int N,K,i,j,sum,start,Max=0;
+    scanf("%d %d",&N,&K);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+        sum = 0;
+        for(j=i;j<N-1;j++)
+        {
+            if(x[j+1]-x[i] <= K) sum=j-i+1;
+            else break;
+        }
+        if(Max<sum) Max=sum;
+    }
+    printf("%d",Max);
+}
