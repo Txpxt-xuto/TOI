a398ebae314672f0b8f08833050a0779d7cbb644
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-23 08:39:01 +0700

    Update TOI5_Number Substitution Wheels.c

--- TOI5_Number Substitution Wheels.c
+++ TOI5_Number Substitution Wheels.c
@@ -8,18 +8,18 @@ CENTER: Home
 #include <string.h>
 int main()
 {
-    int input[3];
-    char password[256];
-    scanf("%d\n%s",&input,&password);
+    char input[3],password[256];
+    scanf("%s\n%s",&input,&password);
     int i,j,k;
     for(i=0;i<strlen(password);i++)
     {
-        printf("%d ",input[0]);
-        printf("%d ",input[1]);
-        printf("%d \n",input[2]);
-        input[0]=(input[0]+(password[i]-'0'))%10;
-        input[1]=(input[0]+(password[i]-'0'))%10;
-        input[2]=(input[1]+(password[i]-'0'))%10;
+        printf("%c ",input[0]);
+        printf("%c ",input[1]);
+        printf("%c \n",input[2]);
+        input[0]=((input[0]-'0')+(password[i]-'0'))%10;
+        input[1]=((input[0]-'0')+(password[i]-'0'))%10;
+        input[2]=((input[1]-'0')+(password[i]-'0'))%10;
+        printf("%d \n",password[i]-'0');
         printf("%d ",input[0]);
         printf("%d ",input[1]);
         printf("%d ",input[2]);
