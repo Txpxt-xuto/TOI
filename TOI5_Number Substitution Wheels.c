cd222e42c06732757867f96bfac1b4d3196ee22e
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-23 08:36:01 +0700

    Update TOI5_Number Substitution Wheels.c

--- TOI5_Number Substitution Wheels.c
+++ TOI5_Number Substitution Wheels.c
@@ -10,14 +10,13 @@ int main()
 {
     int input[3];
     char password[256];
-    scanf("%s\n%s",&input,&password);
+    scanf("%d\n%s",&input,&password);
     int i,j,k;
     for(i=0;i<strlen(password);i++)
     {
         printf("%d ",input[0]);
         printf("%d ",input[1]);
-        printf("%d ",input[2]);
-        printf("%d ",input[3]);
+        printf("%d \n",input[2]);
         input[0]=(input[0]+(password[i]-'0'))%10;
         input[1]=(input[0]+(password[i]-'0'))%10;
         input[2]=(input[1]+(password[i]-'0'))%10;
