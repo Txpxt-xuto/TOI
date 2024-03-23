fce0f5bab637ff2f2861b2c80b9f4ff92d5967b6
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-23 08:54:30 +0700

    Update TOI5_Number Substitution Wheels.c

--- TOI5_Number Substitution Wheels.c
+++ TOI5_Number Substitution Wheels.c
@@ -8,17 +8,27 @@ CENTER: Home
 #include <string.h>
 int main()
 {
+    int i,j,k;
     char input[3],password[256],code[256];
     scanf("%s\n%s",&input,&password);
-    code[256]=password[256];
-    int i,j,k;
+    for(i=0;i<strlen(password);i++)
+    {
+        code[i]=password[i];
+    }
+    
     for(i=0;i<strlen(password);i++)
     {
         for(j=0;j<3;j++)
         {
             password[i]=((input[j]+password[i]-'0'-'0'-2)%9+1)+'0';
-            if(j==0|j==2) input[j] = ((input[j]+code[i]-'0'-'0'-1)%9+1)+'0';
-            else input[j]=((input[j]+7-'0')%9+1)+'0';
+            if(j==0|j==2)
+            {
+                input[j] = ((input[j]+code[i]-'0'-'0'-1)%9+1)+'0';
+            }
+            else
+            {
+                input[j]=((input[j]+7-'0')%9+1)+'0';
+            }
         }
     }
     for(i=0;i<strlen(password);i++)
