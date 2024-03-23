820a8377fe500642ae515d7f397fb437ac3f4fda
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-23 14:47:55 +0700

    Update TOI5_Happy Land.c

--- TOI5_Happy Land.c
+++ TOI5_Happy Land.c
@@ -4,3 +4,29 @@ LANG: C
 AUTHOR: Tapat Toungsakul
 CENTER: Home
 */
+#include <stdio.h>
+int main()
+{
+    int M,N,i,j,x,y;
+    scanf("%d %d",&M,&N);
+    double land[M][N],min=9999999999999;
+    for(i=0;i<M;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            scanf("%f",&land[i][j]);
+        }
+    }
+    for(i=0;i<M;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            if(min<land[i][j])
+            {
+                min = land[i][j];
+                x=i;
+                y=j;
+            }
+        }
+    }
+}
