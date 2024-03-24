94025f2aa3fd74a2a9c9f89b0451cc562b292fbe
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-24 20:08:25 +0700

    Update TOI5_Happy Land.c
    25/100

--- TOI5_Happy Land.c
+++ TOI5_Happy Land.c
@@ -5,51 +5,43 @@ AUTHOR: Tapat Toungsakul
 CENTER: Home
 */
 #include <stdio.h>
-int M,N,i,j,k,x,y,check[10];
-double land[3][3],min=999999999,sum=0,l;
-void place(int k,double x){
-    if(k==M*N)
-    {
-        if(min>x) min = x;
-        return;
-    }
-    for(int i=0;i<M*N;i++)
-    {
-        if(check[i]==1) continue;
-        i=(i/N)+1,j=(i%N)+1;
-        l=land[i][j]/10;
-        check[i]=1;
-        land[i+1][j]+=l;
-        land[i-1][j]+=l;
-        land[i][j+1]+=l;
-        land[i][j-1]+=l;
-        land[i+1][j-1]+=l;
-        land[i+1][j+1]+=l;
-        land[i-1][j-1]+=l;
-        land[i-1][j+1]+=l;
-        place(k+1,x+land[i][j]);
-        check[i]=0;
-        land[i+1][j]-=l;
-        land[i-1][j]-=l;
-        land[i][j+1]-=l;
-        land[i][j-1]-=l;
-        land[i+1][j-1]-=l;
-        land[i+1][j+1]-=l;
-        land[i-1][j-1]-=l;
-        land[i-1][j+1]-=l;
-    }
-}
 int main()
 {
+    int M,N,i,j,k,x,y;
+    double land[5][5],min,sum=0;
     scanf("%d %d",&M,&N);
-    for(i=0;i<M;i++)
+    for(i=1;i<=M;i++)
     {
-        for(j=0;j<N;j++)
+        for(j=1;j<=N;j++)
         {
             scanf("%lf",&land[i][j]);
         }
     }
-    place(0,0);
-    printf("sum = %.2lf",sum);
-    printf("sum = %.2lf",min);
+    for(k=0;k<(M*N);k++)
+    {
+        min=999999999;
+        for(i=1;i<=M;i++)
+        {  
+            for(j=1;j<=N;j++)
+            {
+                if(min>land[i][j] && land[i][j]>=1)
+                {
+                    min = land[i][j];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        sum+=land[x][y];
+        land[x+1][y]+=land[x][y]/10;
+        land[x-1][y]+=land[x][y]/10;
+        land[x][y+1]+=land[x][y]/10;
+        land[x][y-1]+=land[x][y]/10;
+        land[x+1][y-1]+=land[x][y]/10;
+        land[x+1][y+1]+=land[x][y]/10;
+        land[x-1][y-1]+=land[x][y]/10;
+        land[x-1][y+1]+=land[x][y]/10;
+        land[x][y]=-999999999;     
+    }
+    printf("%.2lf",sum);
 }
