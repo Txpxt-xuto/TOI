88878ed0c117069befdf6368f9f5a4f71d98efd0
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-25 14:07:21 +0700

    Update TOI6_Hotel.c

--- TOI6_Hotel.c
+++ TOI6_Hotel.c
@@ -7,4 +7,31 @@ CENTER: Home
 #include <stdio.h>
 int main()
 {
+    int n,i,sum=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        if(n>=9)
+        {
+            n-=15;
+            sum+=3000;
+        }
+        else if(n>=4)
+        {
+            n-=5;
+            sum+=1500;
+        }
+        else if(n==2)
+        {
+            n-=2;
+            sum+=800;
+            
+        }
+        else if(n==1)
+        {
+            n-=1;
+            sum+=500;
+        }
+    }
+    printf("%d",sum);
 }
