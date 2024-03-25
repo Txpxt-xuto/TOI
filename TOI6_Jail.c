241afc6c08704fa31ddd03b60a4bc8f876046d22
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-25 20:24:38 +0700

    Update TOI6_Jail.c

--- TOI6_Jail.c
+++ TOI6_Jail.c
@@ -7,8 +7,28 @@ CENTER: Home
 #include <stdio.h>
 int main()
 {
-    int i,j,n,m;
+    int i,j,k=0,n,m;
     scanf("%d %d",&n,&m);
-    int 
+    int Jail[n],a[n];
+    for(i=0;i<n;i++)
+    {
+        Jail[i]=i;
+    }
+    for(i=0;i<n;i++)
+    {
+        k+=m;
+        printf("%d",Jail[k%(n-i+1)]);
+        Jail[k%(n-i+1)]=0;
+        for(k=0;k<n;k++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(Jail[j]!=0)
+                {
+                    a[k]=Jail[j]
+                }
+            }
+        }
+    }
 
 }
