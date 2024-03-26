109604d4cd3121cf9eca3fb6efd5e80bda884f92
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-26 21:16:09 +0700

    Create TOI6_Gem.c

---
+++ TOI6_Gem.c
@@ -0,0 +1,16 @@
+/*
+TASK: gem
+LANG: C
+AUTHOR: Tapat Toungsakul
+CENTER: Home
+*/
+#include <stdio.h>
+int main()
+{
+    int linear,n[5],m[5];
+    int Gem[5][n[5]*2],danger[5][m[5]];
+    for()
+    {
+        scanf("%d",&)
+    }
+}
