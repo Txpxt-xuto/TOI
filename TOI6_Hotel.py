dbfdb69e6d1f5a1f157b5aa4abe068b53257a76d
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-28 16:16:11 +0700

    Update TOI6_Hotel.py

--- TOI6_Hotel.py
+++ TOI6_Hotel.py
@@ -4,29 +4,18 @@ LANG: C
 AUTHOR: Tapat Toungsakul
 CENTER: Home
 """
-int n,i,sum=0;
-scanf("%d",&n);
-while(n>0)
-{
-    if(n>=9)
-    {
-        n-=15;
-        sum+=3000;
-    }
-    else if(n>=4)
-    {
-        n-=5;
-        sum+=1500;
-    }
-    else if(n>=2)
-    {
-        n-=2;
-        sum+=800;
-    }
-    else if(n==1)
-    {
-        n-=1;
-        sum+=500;
-    }
-}
-printf("%d",sum);
+n=int(input())
+while n>0 :
+    if n>=9 :
+        n-=15
+        sum=sum+3000
+    elif n>=4 :
+        n-=5
+        sum+=1500
+    elif n>=2 :
+        n-=2
+        sum+=800
+    elif n==1 :
+        n-=1
+        sum+=500
+print(sum)
