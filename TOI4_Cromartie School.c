e1686592cf6e61bd034b2e6aad7c49e72b57c823
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-30 15:20:03 +0700

    Update TOI4_Cromartie School.c

--- TOI4_Cromartie School.c
+++ TOI4_Cromartie School.c
@@ -7,35 +7,4 @@ CENTER: Home
 #include <stdio.h>
 int main()
 {
-    int P,i,j,k;
-    scanf("%d",&P);
-    int a,pointodd=P,pointeven=P;
-    for(i=0;i<2*P;++i)
-    {
-        scanf("%d",&a);
-        if(a%2==0)
-        { 
-            j=0;
-            k++;
-            if(k>2) pointeven-=3;
-            if(pointeven<=0) 
-            {
-                printf("0\n%d",a);
-                break;
-            }
-            else pointeven-=1;
-        }
-        else
-        {
-            j++;
-            k=0;
-            if(j>2) pointodd-=3;
-            if(pointodd<=0)
-            {
-                printf("1\n%d",a);
-                break;
-            } 
-            else pointodd-=1;
-        }
-    }
 }
