e4b2ef06c7e5936c34351cead3551cfa12d1c924
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-30 14:52:04 +0700

    Update TOI2_Word.c

--- TOI2_Word.c
+++ TOI2_Word.c
@@ -5,20 +5,6 @@ AUTHOR: Tapat Toungsakul
 CENTER: Home
 */
 #include <stdio.h>
-int main()//กุลีแห่งท่าเรือ//
+int main()
 {
-    long long i,k,N,M;
-    scanf("%d %d",&M,&N);
-    long long Labor[M+3];
-    for(k=1;k<=M;k++) scanf("%d",&Labor[k]);
-    long long k = 1, j = 1000000LL * N;
-    while(k<j)
-    {
-    long long mid = (k + j) / 2;
-    long long all = 0;
-    for (i=1;i<=M;i++) all += mid / Labor[i];
-    if (all>=N) j = mid;
-    else k = mid + 1;
-    }
-    printf("%lld",k);
 }
