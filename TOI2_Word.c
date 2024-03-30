6a8a2456401b91a1335dc50faf8de9a5d54f37dc
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-30 14:46:14 +0700

    Update TOI2_Word.c

--- TOI2_Word.c
+++ TOI2_Word.c
@@ -5,29 +5,20 @@ AUTHOR: Tapat Toungsakul
 CENTER: Home
 */
 #include <stdio.h>
-int main()
+int main()//กุลีแห่งท่าเรือ//
 {
-    int n,m,i,j,k,x;
-    scanf("%d %d",&n,&m);
-    char word[n][m];
-    for(i=0;i<n;i++)
+    long long i,k,N,M;
+    scanf("%d %d",&M,&N);
+    long long Labor[M+3];
+    for(k=1;k<=M;k++) scanf("%d",&Labor[k]);
+    long long k = 1, j = 1000000LL * N;
+    while(k<j)
     {
-        scanf("%s",&word[i]);
+    long long mid = (k + j) / 2;
+    long long all = 0;
+    for (i=1;i<=M;i++) all += mid / Labor[i];
+    if (all>=N) j = mid;
+    else k = mid + 1;
     }
-    scanf("%d",&x);
-    char sentense[x][15];
-    for(i=0;i<x;i++)
-    {
-        scanf("%s",&sentense[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<m;j++)
-        {
-            if(word[i][j]==sentense[i][j])
-            {
-
-            }
-        }
-    }
+    printf("%lld",k);
 }
