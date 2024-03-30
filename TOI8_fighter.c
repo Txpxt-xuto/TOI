2f79c535c7d354ab1d6b9aaf3597569b74d239ce
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-30 20:38:01 +0700

    Final
    100/100

--- TOI8_fighter.c
+++ TOI8_fighter.c
@@ -6,33 +6,28 @@ AUTHOR: Tapat Toungsakul
 CENTER: Home
 */ 
 #include <stdio.h>
-
 int main() {
     int n;
-
     scanf("%d", &n);
-
     int ca = 1, cb = 1, a = n, b = n, i = 0, t;
-
     while (a > 0 && b > 0 && i < 2*n) {
         scanf("%d", &t);
-
-        if (t%2==0) {
+        if (t%2==0)
+        {
             cb = 1;
             if (ca >= 3) b-=3;
             else b--;
-
             ca++;
-        } else {
+        } 
+        else
+        {
             ca = 1;
             if (cb >= 3) a-=3;
             else a--;
-
             cb++;
         }
         i++;
     }
-
     if (ca > cb) printf("%d\n%d", 0, t);
     else printf("%d\n%d", 1, t);
 }
