c8c7c9243e71eb71a9f0c229f12ea8a227c01a7c
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-31 13:46:27 +0700

    Update TOI10_Prime.c

--- TOI10_Prime.c
+++ TOI10_Prime.c
@@ -1,11 +1,25 @@
 /*
-TASK: fighter.c
+TASK: prime.c 
 LANG: C
-COMPILER: WDC
 AUTHOR: Tapat Toungsakul
 CENTER: Home
 */ 
 #include <stdio.h>
 int main()
 {
+    int n,i,j=1,k,sum=0;
+    scanf("%d",&n);
+    while(sum<n)
+    {
+        j++;
+        k=0;
+        for(i=1;i<j;i++)
+        {
+            if(j%i!=0 || j==2 || i==1) k++;
+            else i=j;
+        }
+        if(k==j-1) sum++;
+        
+    }
+    printf("%d",j);
 }
