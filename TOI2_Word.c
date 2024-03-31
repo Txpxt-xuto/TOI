0ad135f8c111e12bcb49a499772e26431a472a8f
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-31 12:53:30 +0700

    Update TOI2_Word.c

--- TOI2_Word.c
+++ TOI2_Word.c
@@ -12,73 +12,95 @@ int main()
 {
     int m,n,k,i,j,x,y,len,ch=0;
     scanf("%d %d",&m,&n);
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
             scanf(" %c",&a[i][j]);
             a[i][j] = toupper(a[i][j]);
         }
     }
     scanf("%d",&k);
-    while(k--){
+    while(k--)
+    {
         ch = 0;
         scanf(" %s",b);
         len = strlen(b);
         for(i=0;i<len;i++)
             b[i] = toupper(b[i]);
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                if(b[0]==a[i][j]){
-                    if(b[1]==a[i+1][j]){
-                        for(x=1;x<len;x++){
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(b[0]==a[i][j])
+                {
+                    if(b[1]==a[i+1][j])
+                    {
+                        for(x=1;x<len;x++)
+                        {
                             if(b[x]==a[i+x][j] && x==len-1) ch++;
                             else if(b[x]!=a[i+x][j]) break;
                         }
                     }
                     if(ch>0) goto next;
-                    if(b[1]==a[i-1][j]){
-                        for(x=1;x<len;x++){
+                    if(b[1]==a[i-1][j])
+                    {
+                        for(x=1;x<len;x++)
+                        {
                             if(b[x]==a[i-x][j] && x==len-1) ch++;
                             else if(b[x]!=a[i-x][j]) break;
                         }
                     }
                     if(ch>0) goto next;
-                    if(b[1]==a[i][j+1]){
-                        for(x=1;x<len;x++){
+                    if(b[1]==a[i][j+1])
+                    {
+                        for(x=1;x<len;x++)
+                        {
                             if(b[x]==a[i][j+x] && x==len-1) ch++;
                             else if(b[x]!=a[i][j+x]) break;
                         }
                     }
                     if(ch>0) goto next;
-                    if(b[1]==a[i][j-1]){
-                        for(x=1;x<len;x++){
+                    if(b[1]==a[i][j-1])
+                    {
+                        for(x=1;x<len;x++)
+                        {
                             if(b[x]==a[i][j-x] && x==len-1) ch++;
                             else if(b[x]!=a[i][j-x]) break;
                         }
                     }
                     if(ch>0) goto next;
-                    if(b[1]==a[i+1][j+1]){
-                        for(x=1;x<len;x++){
+                    if(b[1]==a[i+1][j+1])
+                    {
+                        for(x=1;x<len;x++)
+                        {
                             if(b[x]==a[i+x][j+x] && x==len-1) ch++;
                             else if(b[x]!=a[i+x][j+x]) break;
                         }
                     }
                     if(ch>0) goto next;
-                    if(b[1]==a[i+1][j-1]){
-                        for(x=1;x<len;x++){
+                    if(b[1]==a[i+1][j-1])
+                    {
+                        for(x=1;x<len;x++)
+                        {
                             if(b[x]==a[i+x][j-x] && x==len-1) ch++;
                             else if(b[x]!=a[i+x][j-x]) break;
                         }
                     }
                     if(ch>0) goto next;
-                    if(b[1]==a[i-1][j+1]){
-                        for(x=1;x<len;x++){
+                    if(b[1]==a[i-1][j+1])
+                    {
+                        for(x=1;x<len;x++)
+                        {
                             if(b[x]==a[i-x][j+x] && x==len-1) ch++;
                             else if(b[x]!=a[i-x][j+x]) break;
                         }
                     }
                     if(ch>0) goto next;
-                    if(b[1]==a[i-1][j-1]){
-                        for(x=1;x<len;x++){
+                    if(b[1]==a[i-1][j-1])
+                    {
+                        for(x=1;x<len;x++)
+                        {
                             if(b[x]==a[i-x][j-x] && x==len-1) ch++;
                             else if(b[x]!=a[i-x][j-x]) break;
                         }
