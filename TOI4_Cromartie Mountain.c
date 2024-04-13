d7d513f8148f4f8c8180c511b86d51f3042404b2
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-04-13 11:27:58 +0700

    Update TOI4_Cromartie Mountain.c

--- TOI4_Cromartie Mountain.c
+++ TOI4_Cromartie Mountain.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() 
 {
-    int n;
+    int n,i,j;
     scanf("%d", &n);
     int len = 0, hei = 0, s[n], h[n];
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         scanf("%d%d", &s[i], &h[i]);
         s[i]--;
         len = len > s[i] - 1 + 2*h[i] ? len : s[i] - 1 + 2*h[i];
@@ -12,28 +12,31 @@ int main()
     }
     len++;
     char map[hei][len];
-    for (int i = 0; i < hei; i++) for (int j = 0; j < len; j++) map[i][j] = '.';
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < hei; i++) for (j = 0; j < len; j++) map[i][j] = '.';
+    for (i = 0; i < n; i++) 
+    {
         int m = s[i], p = hei-1;
-        while (m <= s[i] + h[i] - 1) {
+        while (m <= s[i] + h[i] - 1) 
+        {
             if (map[p][m] == '.') map[p][m] = '/';
             else if (map[p][m] == '\\') map[p][m] = 'v';
-            for (int j = p+1; j < hei; j++) map[j][m] = 'X';
+            for (j = p+1; j < hei; j++) map[j][m] = 'X';
             m++;
             p--;
         }
         p++;
-        while (m <= s[i] + 2*(h[i]) - 1) {
+        while (m <= s[i] + 2*(h[i]) - 1) 
+        {
             if (map[p][m] == '.') map[p][m] = '\\';
             else if (map[p][m] == '/') map[p][m] = 'v';
-            for (int j = p+1; j < hei; j++) map[j][m] = 'X';
+            for (j = p+1; j < hei; j++) map[j][m] = 'X';
             m++;
             p++;
         }
     }
-    for (int i = 0; i < hei; i++) 
+    for (i = 0; i < hei; i++) 
     {
-        for (int j = 0; j < len; j++) printf("%c", map[i][j]);
+        for (j = 0; j < len; j++) printf("%c", map[i][j]);
         printf("\n");
     }
 }
