c5f4fdc3c8ca7db14d1a2214702b5afa0b4f5154
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-04-13 11:36:39 +0700

    Create TOI4_Temperature is Rising.py

---
+++ TOI4_Temperature is Rising.py
@@ -0,0 +1,22 @@
+size = int(input())
+x,y = [int(i) for i in input().split()]
+grid = [[100 for _ in range(size+2)]] + [[100]+[int(i) for i in input().split()]+[100] for _ in range(size)] + [[100 for _ in range(size+2)]]
+
+cango = []
+
+def solve(x,y):
+    global cango
+    number = grid[y][x]
+    if number == 100:
+        return
+    cango.append(number)
+    if number < grid[y][x+1]:
+        solve(x+1,y)
+    if number < grid[y][x-1]:
+        solve(x-1,y)
+    if number < grid[y+1][x]:
+        solve(x,y+1)
+    if number < grid[y-1][x]:
+        solve(x,y-1)
+solve(x,y)
+print(max(cango))
