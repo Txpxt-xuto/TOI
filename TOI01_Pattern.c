f7f8baa827860df86ed80083131ece6ebd15c9b2
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-04-26 12:01:02 +0700

    Final
    100/100

--- TOI01_Pattern.c
+++ TOI01_Pattern.c
@@ -13,26 +13,17 @@ int main()
 	scanf("%d",&N);
 	for(i=1;i<=50000;i++)
     {
-		for(j=1;j<=70;j++)
-        {
-			PATTERN[i][j] = 'o';
-		}
+		for(j=1;j<=70;j++) PATTERN[i][j] = 'o';
 	}
-    for(i=0;i<N;i++){
+    for(i=0;i<N;i++)
+	{
 		scanf("%d %d %d",&x,&y,&z);
-		for(j=0;j<z;j++){
-			PATTERN[x][y+j] = 'x';
-		}
-		if(x > max){
-			max = x;
-		}
+		for(j=0;j<z;j++) PATTERN[x][y+j] = 'x';
+		if(x > max) max = x;
 	}
     for(i=1;i<=max;i++)
     {
-		for(j=1;j<=70;j++)
-        {
-			printf("%c",PATTERN[i][j]);
-		}
+		for(j=1;j<=70;j++) printf("%c",PATTERN[i][j]);
 		printf("\n");
 	}
 }
