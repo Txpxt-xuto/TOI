88581e3d6935c746db544d10a3a18aa32a722fb2
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-04-27 13:46:12 +0700

    Final
    100/100

---
+++ TOI06_Jail.c
@@ -0,0 +1,28 @@
+/*
+TASK: jail
+LANG: C
+AUTHOR: Tapat Toungsakul
+CENTER: Home
+*/
+#include <stdio.h>
+int main()
+{
+    int i,j=0,k=1,n,m;
+    scanf("%d %d",&n,&m);
+    int Jail[n];
+    for(i=0;i<n;i++) Jail[i]=0;
+    i=0;
+    while(j!=n)
+    {
+        if(i>=n) i-=n;
+        if(k%m==0 && Jail[i]==0) 
+        {
+            printf("%d ",i+1);
+            Jail[i]=1;
+            k++;
+            j++;
+        } 
+        else if(Jail[i]!=1) k++;
+        i++;
+    }
+}
