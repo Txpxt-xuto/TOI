6feb83ba8874d16268882f83f71ef15065c9dfda
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-01 15:29:00 +0700

    Update TOI07_Space.c

--- TOI07_Space.c
+++ TOI07_Space.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
-int d;
-int memory[d];
-void stock(int arr[], int n)
+#include <math.h>
+void stock(int arr[], int d)
 {
-    for (int i = 0; i < n; i++) 
+    for(int i = 0; i < d-1; i++) 
     {
-        cout << arr[i] << " ";
+        for(int j = 0; j < d; j++) 
+        {
+            if(arr[i+1]-arr[i]==10)
+            {
+
+            }
+        }
     }
 }
-void generatebit(int n, int arr[], int i)
+void generatebit(int d, int arr[], int i)
 {
-    if (i == n) 
+    if(i == d) 
     {
-        stock(arr, n);
+        stock(arr, d);
         return;
     }
     arr[i] = 0;
-    generatebit(n, arr, i + 1);
+    generatebit(d, arr, i + 1);
     arr[i] = 1;
-    generatebit(n, arr, i + 1);
+    generatebit(d, arr, i + 1);
 }
 int main()
 {
+    int d;
     int arr[d];
     scanf("%d",&d);
     generatebit(d, arr, 0);
