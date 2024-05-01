60906677e44e50ab3c0ed5b984d8ccfc7f13ff78
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-01 15:56:18 +0700

    Update TOI07_Space.c

--- TOI07_Space.c
+++ TOI07_Space.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-int generatebit(int d, int arr[], int i, int memory[])
+int generatebit(int d, int arr[], int i)
 {
     
     if(i == d) 
     {
         for(int j = 0; j < d-1; j++) 
         {
-            return memory;
+            return ;
         }
         return;
     }
-    int k;
     arr[i] = 0;
-    generatebit(d, arr, i + 1, memory);
-    k++;
+    generatebit(d, arr, i + 1);
     arr[i] = 1;
-    generatebit(d, arr, i + 1, memory);
-    k++;
+    generatebit(d, arr, i + 1);
 }
 int main()
 {
     int d;
     int arr[d],memory[d*d][d];
     scanf("%d",&d);
-    generatebit(d, arr, 0, memory);
-    return 0;
+    generatebit(d, arr, 0);
 }
