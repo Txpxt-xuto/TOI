b88de3ece46d1b28348fdb60481529d33a1e6a59
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-01 16:13:06 +0700

    Update TOI07_Space.c

--- TOI07_Space.c
+++ TOI07_Space.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
-void generatebit(int d)
+void generatebit(int d, string)
 {
-    
+    if(n==d) 
+    {
+        for(int i=d-1;i>=0;i--) {
+            if(s[i]=='0') 
+            {
+                cout<<s<<" "; 
+                s[i]='1'
+                ; cout<<s<<"\n"
+                ; s[i]='0';
+            }
+        }
+    }
+    else
+    {
+        solve(n+1,s+'0');
+        solve(n+1,s+'1');
+    }
 }
 int main()
 {
