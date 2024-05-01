babdba908e2fbf249f40dcc484d36db2d6452559
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-01 15:49:50 +0700

    Update TOI07_Space.c

--- TOI07_Space.c
+++ TOI07_Space.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 #include <math.h>
-void stock(int arr[], int d)
+int generatebit(int d, int arr[], int i, int memory[])
 {
-    for(int i = 0; i < d-1; i++) 
-    {
-        for(int j = 0; j < d; j++) 
-        {
-            if(arr[i+1]-arr[i]==pow(10, j))
-            {
-                printf("%d %d\n",arr[i],arr[i+1]);
-            }
-        }
-    }
-}
-void generatebit(int d, int arr[], int i)
-{
+    int k;
     if(i == d) 
     {
-        stock(arr, d);
+        for(int j = 0; j < d-1; j++) 
+        {
+            return memory[k];
+        }
         return;
     }
     arr[i] = 0;
-    generatebit(d, arr, i + 1);
+    generatebit(d, arr, i + 1, memory);
+    k++;
     arr[i] = 1;
-    generatebit(d, arr, i + 1);
+    generatebit(d, arr, i + 1, memory);
+    k++;
 }
 int main()
 {
     int d;
-    int arr[d];
+    int arr[d],memory[d*d][d];
     scanf("%d",&d);
-    generatebit(d, arr, 0);
+    generatebit(d, arr, 0, memory);
     return 0;
 }
