ab90b5c4685621cebf16cfdb6b2a92ed51ae660e
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-10 20:25:19 +0700

    Update TOI08_location.c

--- TOI08_location.c
+++ TOI08_location.c
@@ -8,25 +8,30 @@ CENTER: Home
 #include <stdio.h>
 int main()
 {
-    int M,N,K,i,j,k,max=0;
+    int M,N,K,i,j;
     scanf("%d %d",&M,&N);
     scanf("%d",&K);
     int arr[M][N],sum[K][K];
     for(i=0;i<M;i++)
     {
-        for(j=0;j<N;j++) scanf("%d",&arr[i][j]);
+        for(j=0;j<N;j++) 
+        {
+            scanf("%d",&arr[i][j]);
+        }
     }
     for(i=0;i<M-K+1;i++)
     {
         for(j=0;j<N-K+1;j++) 
         {
-            for(int x=i;x<K;x++)
+            int max=0;
+            for(int x=i;x<i+K;x++)
             {
-                for(int y=j;y<K;y++) 
+                for(int y=j;y<j+K;y++) 
                 {
-                    max+=sum[i][j];
+                    max+=sum[x][y];
                 }
             }
+            printf("%d\n",max);
         }
     }
 }
