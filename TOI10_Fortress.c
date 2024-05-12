1f0d4b9b8d5b8e5f89250732af728d07c978b2d0
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-12 14:38:20 +0700

    Create TOI10_Fortress.c

---
+++ TOI10_Fortress.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+  int z[20],n[20];
+  for(int i=0;i<20;i++)
+  {
+      if(z[i]<=200000000||n[i]<=100000000)
+      {
+      scanf("%d %d",&z[i],&n[i]);
+      if(n[i]*2>z[i]||z[i]%2!=0||n[i]==0||z[i]==0||n[i]<(z[i]-n[i]*2)/2)
+      {
+      printf("%d\n",0);
+      }
+      else
+      {
+      printf("%d\n",n[i]-((z[i]-n[i]*2)/2)+1);
+      }
+      }
+  }
+}
