a458c97de3ef3fa1dcc4edf27cb6f0797d1b332f
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-12 14:41:28 +0700

    Update TOI10_Fortress.c

--- TOI10_Fortress.c
+++ TOI10_Fortress.c
@@ -13,7 +13,7 @@ int main()
     if(dp[i]<=200000000||n[i]<=100000000)
     {
       scanf("%d %d",&dp[i],&n[i]);
-      if(n[i]*2>dp[i] || dp[i]%2!=0 || n[i]==0 || (dp[i]==0) || (n[i]<(dp[i]-n[i]*2)/2)) printf("%d\n",0);
+      if((n[i]*2>dp[i]) || (dp[i]%2!=0) || (n[i]==0) || (dp[i]==0) || (n[i]<(dp[i]-n[i]*2)/2)) printf("%d\n",0);
       else printf("%d\n",n[i]-((dp[i]-n[i]*2)/2)+1);
     }
   }
