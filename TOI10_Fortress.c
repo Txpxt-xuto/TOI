94fefaa64afe229633ef84bebc3ab2e7dd2cda45
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-12 14:41:54 +0700

    Final
    100/100

--- TOI10_Fortress.c
+++ TOI10_Fortress.c
@@ -7,10 +7,10 @@ CENTER: Home
 #include <stdio.h>
 int main() 
 {
-  int dp[21],n[21];
-  for(int i=0;i<20;i++)
+  int dp[21],n[21],i;
+  for(i=0;i<20;i++)
   {
-    if(dp[i]<=200000000||n[i]<=100000000)
+    if(dp[i]<=200000000 || n[i]<=100000000)
     {
       scanf("%d %d",&dp[i],&n[i]);
       if((n[i]*2>dp[i]) || (dp[i]%2!=0) || (n[i]==0) || (dp[i]==0) || (n[i]<(dp[i]-n[i]*2)/2)) printf("%d\n",0);
