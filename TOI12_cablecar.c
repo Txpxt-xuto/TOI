bac12d25ccfe7753872db207cf8e51c6e7e6e3c4
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-13 19:25:45 +0700

    Update TOI12_cablecar.c

--- TOI12_cablecar.c
+++ TOI12_cablecar.c
@@ -58,8 +58,8 @@ Triple pop(PriorityQueue* pq)
         if (pq->array[leftChild].first > pq->array[maxIndex].first) {
             maxIndex = leftChild;
         }
-        if (rightChild < pq->size && pq->array[rightChild].first > pq->array[maxIndex].first) {
-            maxIndex = rightChild;
+        if (rightChild < pq->size && pq->array[rightChild].first > pq->array[maxIndex].first) maxIndex = rightChild;
+
         }
         if (maxIndex == i) break;
         swap(&pq->array[i], &pq->array[maxIndex]);
