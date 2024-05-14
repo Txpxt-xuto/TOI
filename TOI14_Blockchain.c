c4085fb821249cf801bc9e915047687085d130c2
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-14 21:14:01 +0700

    Update TOI14_Blockchain.c

--- TOI14_Blockchain.c
+++ TOI14_Blockchain.c
@@ -5,80 +5,53 @@ AUTHOR: Tapat Toungsakul
 CENTER: Home
 */
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+int main()//BLOCKCHAIN//
+{
+    int a,b,x,y,i,j,k,l,t,q;
+    scanf("%d %d",&t,&q);
+    int T[t][100][2],Q[q][100][2],n[t],m[q];
+    for(i=0;i<t;i++)//ถูกตรวจสอบ//
+    {
+        scanf("%d",&n[i]);
+        for(j=0;j<n[i]-1;j++)
+        {
+            scanf("%d %d",&T[i][j][0],&T[i][j][1]);
+        }
+    }
+    for(i=0;i<q;i++)//ตัวตรวจสอบ//
+    {
+        scanf("%d",&m[i]);
+        for(j=0;j<m[i]-1;j++)
+        {
+            scanf("%d %d",&Q[i][j][0],&Q[i][j][1]);
+        }
 
-#define MAX_NODES 10000
-
-typedef struct {
-    int u;
-    int v;
-} Edge;
-
-typedef Edge EdgeList[MAX_NODES];
-
-int input() {
-    int val;
-    scanf("%d", &val);
-    return val;
-}
-
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void sort_edge(EdgeList vec, int size) {
-    for (int i = 0; i < size; ++i) {
-        for (int j = i + 1; j < size; ++j) {
-            if (vec[i].u > vec[j].u || (vec[i].u == vec[j].u && vec[i].v > vec[j].v)) {
-                swap(&vec[i].u, &vec[j].u);
-                swap(&vec[i].v, &vec[j].v);
+    }
+    for(x=0;x<q;x++)//ตัวตรวจสอบ ตรวจ ตัวถูกตรวจสอบ//
+    {
+        l=0;
+        for(i=0;i<t;i++)
+        {
+            if(m[x]==n[i])
+            {
+                k=0;
+                for(y=0;y<m[x]-1;y++)
+                {
+                    for(j=0;j<n[i]-1;j++)
+                    {
+                        if((Q[x][y][0]==T[i][j][0] && Q[x][y][1]==T[i][j][1]) || (Q[x][y][1]==T[i][j][0] && Q[x][y][0]==T[i][j][1]))
+                        {
+                            k++;
+                            break;
+                        }
+                    }
+                }
+                if(k==m[x]-1)
+                {
+                    l++;
+                }
             }
         }
+        printf("\n%d",l);
     }
-}
-
-void get_tree_graph(EdgeList vec, int *size) {
-    int n = input();
-    for (int i = 1; i < n; ++i) {
-        int u = input();
-        int v = input();
-        if (u < v) {
-            swap(&u, &v);
-        }
-        vec[*size].u = u;
-        vec[*size].v = v;
-        (*size)++;
-    }
-    sort_edge(vec, *size);
-}
-
-int main() {
-    int t = input();
-    int q = input();
-    EdgeList trees[MAX_NODES];
-    int tree_counts[MAX_NODES] = {0};
-
-    for (int i = 0; i < t; ++i) {
-        int size = 0;
-        get_tree_graph(trees[i], &size);
-        tree_counts[i]++;
-    }
-
-    for (int i = 0; i < q; ++i) {
-        int size = 0;
-        EdgeList query;
-        get_tree_graph(query, &size);
-        int count = 0;
-        for (int j = 0; j < t; ++j) {
-            if (memcmp(trees[j], query, sizeof(Edge) * size) == 0) {
-                count += tree_counts[j];
-            }
-        }
-        printf("%d\n", count);
-    }
-
-    return 0;
 }
