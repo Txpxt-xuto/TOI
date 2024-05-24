3382aa0707ebb18d7ef95d5bb9b9b6b7c14f1b6b
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-24 18:44:06 +0700

    Update TOI14_NBK48.c

--- TOI14_NBK48.c
+++ TOI14_NBK48.c
@@ -8,9 +8,7 @@ CENTER: Home
 
 int main() {
     int n, q;
-
     scanf("%d%d", &n, &q);
-
     int a[n], b[q];
     for (int i = 0; i < n; i++) scanf("%d", &a[i]);
     for (int i = 0; i < q; i++) {
