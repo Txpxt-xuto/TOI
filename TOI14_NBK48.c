8d80f09160eb331eeaa091044bc05a486f9ecb16
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-05-24 18:43:46 +0700

    Update TOI14_NBK48.c

--- TOI14_NBK48.c
+++ TOI14_NBK48.c
@@ -5,8 +5,28 @@ AUTHOR: Tapat Toungsakul
 CENTER: Home
 */
 #include <stdio.h>
-#include <math.h>
-int main()
-{
-    
+
+int main() {
+    int n, q;
+
+    scanf("%d%d", &n, &q);
+
+    int a[n], b[q];
+
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
+    for (int i = 0; i < q; i++) {
+        int count = 0, money;
+        
+        scanf("%d", &money);
+
+        for (int j = 0; j < n; j++) {
+            if (money - a[j] >= 0) {
+                count = j+1;
+            }
+            money -= a[j];
+        }
+
+        printf("%d\n", count);
+    }
 }
