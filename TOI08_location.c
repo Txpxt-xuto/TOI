2fe5d508d0960f9e08dce3b73f1d9ab0132357a6
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-06-09 20:46:21 +0700

    Update TOI08_location.c

--- TOI08_location.c
+++ TOI08_location.c
@@ -6,27 +6,35 @@ AUTHOR: Tapat Toungsakul
 CENTER: Home
 */ 
 #include <stdio.h>
-int main()
-{
-    int M,N,K,i,j,k;
-    scanf("%d %d",&M,&N,&K);
-    scanf("%d",&K);
-    int arr[M][N];
-    for(i=0;i<M;i++)
-    {
-        for(j=0;j<N;j++) scanf("%d",&arr[i][j]);
-    }
-    for(i=0;i<M-K+1;i++)
-    {
-        for(j=0;j<N-K+1;j++) 
-        {
-            int max=0;
-            for(int x=i;x<i+K;x++)
-            {
-                for(int y=j;y<j+K;y++) max+=arr[x][y];
+
+int main( ) {
+    int m, n;
+    scanf("%d%d", &m, &n);
+
+    int k;
+    scanf("%d", &k);
+
+    int map[m][n];
+
+    for (int i = 0; i < m; i++) for (int j = 0; j < n; j++) scanf("%d", &map[i][j]);
+
+    int ans = 0;
+
+    for (int i = 0; i <= m-k; i++) {
+        for (int j = 0; j <= n-k; j++) {
+            int temp = 0;
+
+            for (int a = i; a < i+k; a++) {
+                for (int b = j; b < j+k; b++) {
+                    temp += map[a][b];
+                }
             }
-            if(max>k) k=max;
+
+            if (temp > ans) {
+                ans = temp;
+            }
         }
     }
-    printf("%d",k);
+
+    printf("%d", ans);
 }
