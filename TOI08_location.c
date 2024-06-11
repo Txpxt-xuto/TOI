11e76a6ad480244e2d86f6affbe045a22f0802ad
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-06-11 20:27:12 +0700

    Update TOI08_location.c

--- TOI08_location.c
+++ TOI08_location.c
@@ -14,7 +14,7 @@ int main()
     int map[m][n];
     for(int i = 0; i < m; i++) for(int j = 0; j < n; j++) scanf("%d", &map[i][j]);
     int ans = 0;
-    for (int i = 0; i <= m-k; i++) 
+    for (int i=0;i<=m-k;i++) 
     {
         for (int j = 0; j <= n-k; j++) 
         {
