5a5ff02167c2ec7a977eabb8635e1ccad9ebef2d
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-06-11 20:28:10 +0700

    Update TOI08_location.c

--- TOI08_location.c
+++ TOI08_location.c
@@ -8,7 +8,7 @@ CENTER: Home
 #include <stdio.h>
 int main()
 {
-    int m, n, k;
+    int m, n, k, a, i, j;
     scanf("%d%d", &m, &n);
     scanf("%d", &k);
     int map[m][n];
@@ -16,11 +16,11 @@ int main()
     int ans = 0;
     for(i=0;i<=m-k;i++) 
     {
-        for (j=0;j<=n-k;j++) 
+        for(j=0;j<=n-k;j++) 
         {
             int temp = 0;
-            for (a=i;a<i+k;a++) for (int b = j; b < j+k; b++) temp += map[a][b];
-            if (temp > ans) ans = temp;
+            for(a=i;a<i+k;a++) for(int b = j; b < j+k; b++) temp += map[a][b];
+            if(temp > ans) ans = temp;
         }
     }
     printf("%d", ans);
