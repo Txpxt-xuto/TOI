7320c2afad3ba00e298e1f674412026c12a55710
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-06-11 20:27:49 +0700

    Update TOI08_location.c

--- TOI08_location.c
+++ TOI08_location.c
@@ -12,14 +12,14 @@ int main()
     scanf("%d%d", &m, &n);
     scanf("%d", &k);
     int map[m][n];
-    for(int i = 0; i < m; i++) for(int j = 0; j < n; j++) scanf("%d", &map[i][j]);
+    for(i=0;i<m;i++) for(int j = 0; j < n; j++) scanf("%d", &map[i][j]);
     int ans = 0;
-    for (int i=0;i<=m-k;i++) 
+    for(i=0;i<=m-k;i++) 
     {
-        for (int j = 0; j <= n-k; j++) 
+        for (j=0;j<=n-k;j++) 
         {
             int temp = 0;
-            for (int a = i; a < i+k; a++) for (int b = j; b < j+k; b++) temp += map[a][b];
+            for (a=i;a<i+k;a++) for (int b = j; b < j+k; b++) temp += map[a][b];
             if (temp > ans) ans = temp;
         }
     }
