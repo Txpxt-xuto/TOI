f934e16c783078f04ec29f60cb48725fc3895d89
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-08-14 15:45:34 +0700

    Update TOI11_Candle.c

--- TOI11_Candle.c
+++ TOI11_Candle.c
@@ -15,9 +15,9 @@ int main(void)
     int count = 0;
     scanf("%d %d", &m, &n);
     for(i=0;i<m;i++) scanf("%s", candle[i]);
-    for(int i = 0; i < m; i++) 
+    for(i=0;i<m;i++) 
     {
-        for(int j = 0; j < n; j++) 
+        for(j=0;j<n;j++) 
         {
             if (candle[i][j] == '0') continue;
             bfs(i, j, m, n);
