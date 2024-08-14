bc74e7e956fd52427c3c305d68ac6b1303da69a0
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-08-14 15:42:30 +0700

    Update TOI11_Candle.c

--- TOI11_Candle.c
+++ TOI11_Candle.c
@@ -4,11 +4,3 @@ LANG: C++
 AUTHOR: YourName YourLastName
 CENTER: YourCenter
 */ 
-#include<bits/stdc++.h>
-using namespace std;
-int n,m,cnt;
-char arr[2000][2000];
-int ;
-int di[8]={-1,-1,0,1,1,1,0,-1};
-int dj[8]={0,1,1,1,0,-1,-1,-1};
-queue<pair<int,int>> q;
