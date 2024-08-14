e0cf64e56014fc68e4835a981fc039df4b383a37
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-08-14 15:42:26 +0700

    Update TOI11_Candle.c

--- TOI11_Candle.c
+++ TOI11_Candle.c
@@ -12,9 +12,3 @@ int ;
 int di[8]={-1,-1,0,1,1,1,0,-1};
 int dj[8]={0,1,1,1,0,-1,-1,-1};
 queue<pair<int,int>> q;
-bool issafe(int i,int j)
-{
-    if(i<0 or i>=n or j<0 or j>=m)return false;
-    if(arr[i][j]=='0')return false;
-    return true;
-}
