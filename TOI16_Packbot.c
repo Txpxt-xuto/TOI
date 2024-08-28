eec89a920bb2e7c7811c08e1e595da7f156c8f39
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-08-28 19:35:59 +0700

    Update TOI16_Packbot.c

--- TOI16_Packbot.c
+++ TOI16_Packbot.c
@@ -19,5 +19,5 @@ n->o=o;n->l=l;n->r=r;return n;} N*E(V)
 ;N*F(V);
 N*G(V);
 N*T(V);
- DEF(E,104,F,'1')
+DEF(E,104,F,'1')
 DEF(F,108,G,'2')DEF(G,116,T,'3') N*T(V){if(!s[p])return 0;if(s[p]=='['){p++;if(s[p]==']'){p++;return T();}N*n=E();p++;return n;}p++;return calloc(4,5);} I O(N*n){if(!n->o)return 20;return((O(n->l)+O(n->r))*n->o)/100;}I main(){scanf("%s",s);printf("%d",O(E()));return 0;}
