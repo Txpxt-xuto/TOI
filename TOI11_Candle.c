4f0db3141ea9c66d75c958ef4c4f7e5878616cd6
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-09-01 15:59:13 +0700

    Update TOI11_Candle.c

--- TOI11_Candle.c
+++ TOI11_Candle.c
@@ -11,7 +11,7 @@ char candle[2000][2000];
 void bfs(int x, int y, int m, int n);
 int main(void) 
 {
-    int m, n, count = 0;
+    int m, n, count = 0, i, j;
     scanf("%d %d", &m, &n);
     for(i=0;i<m;i++) scanf("%s", candle[i]);
     for(i=0;i<m;i++) 
