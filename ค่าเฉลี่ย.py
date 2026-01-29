e754d8a0155d07fa57fd7dfca8ef5e765f13969a
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2026-01-29 09:42:38 +0700

    Update ค่าเฉลี่ย.py

--- ค่าเฉลี่ย.py
+++ ค่าเฉลี่ย.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 men_means, men_std = (7.0, 6.2, 8.1), (0.5, 0.5, 0.5)
 ind = np.arange(len(men_means))  # ตำแหน่งของกลุ่ม
 width = 0.35  # ความกว้างของแท่งกราฟ
-
 fig, ax = plt.subplots()
 rects1 = ax.bar(ind - width/2 , men_means, width, yerr=men_std, color='SkyBlue', label='pH')
 # กำหนดข้อความที่แสดงบริเวณด้านข้างกับด้านบนรวมถึงการกำหนดชื่อข้อมูลในแต่ละตัว
