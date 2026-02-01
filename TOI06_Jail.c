44677a884ec059e0eb24a681be43311696fb965e
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2026-02-01 20:42:41 +0700

    Update TOI06_Jail.c

--- TOI06_Jail.c
+++ TOI06_Jail.c
@@ -30,22 +30,6 @@ int main()
 #include <stdio.h>
 int main()
 {
-    int i,j=0,k=1,n,m;
-    scanf("%d %d",&n,&m);
-    int Jail[n];
-    for(i=0;i<n;i++) Jail[i]=0;
-    i=0;
-    while(j!=n)
-    {
-        if(i>=n) i-=n;
-        if(k%m==0 && Jail[i]==0) 
-        {
-            printf("%d ",i+1);
-            Jail[i]=1;
-            k++;
-            j++;
-        } 
-        else if(Jail[i]!=1) k++;
-        i++;
-    }
+    int i,j,k;
+    char Kemija
 }
