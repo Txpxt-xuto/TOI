a5c8de5fc99f2c312af6647fc96572558cf0435c
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2026-02-05 19:49:05 +0700

    Update TOI06_Jail.c

--- TOI06_Jail.c
+++ TOI06_Jail.c
@@ -95,8 +95,15 @@ int main()
 {
     int i,j,k,N;
     scanf("%d",N);
-    for(i=0;i<N;i++)
+    for(i=1;i<=N;i++)
     {
-
+        for(j=1;j<=N-1;j++)
+        {
+            if()
+            {
+                printf(*);
+            }
+            else printf("-");
+        }
     }
 }
